7352f0fecd3033f12e0171d463bc037363edfcb6
mahyar1284 <mahyarshokraeian@live.com>
2026-01-20 03:27:53 +0330

    add "isEndstopAtClosedState" option

--- src/MotionVisorConfig.hpp
+++ src/MotionVisorConfig.hpp
@@ -5,6 +5,7 @@ struct MotionVisorConfig
 {
     bool invertDir = true; // based on motor connection
     bool invertEndstopPin = true; // based on sensor type
+    bool isEndstopAtClosedState = true; // based on endstop placement (Not Implemented Yet!!!!)
     double endstopMinDistance = 5; // minimum required distance to release endstop
     double stepPermm = 200.0; // step count per each mm of movement
     double speed = 8; // mm/s
