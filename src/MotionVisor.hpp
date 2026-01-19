786eabf67cb8f40e2d48fea6a82b4d28a08a4de1
mahyar1284 <mahyarshokraeian@live.com>
2026-01-20 03:16:41 +0330

    update gpio pins

--- src/MotionVisor.hpp
+++ src/MotionVisor.hpp
@@ -26,7 +26,7 @@ private:
 
     HardwareTimer timer;
     MotionVisorState _state = MotionVisorState::Uninitialized;
-    int dirPin = PC14, stepPin = PC15, enPin = PB4, endstopPin = PA8;
+    int dirPin = PC14, stepPin = PC15, enPin = PB0, endstopPin = PB1;
     MotionVisorConfig config;
     long goalStep = 0;
     long totalDistSteps = 0;
@@ -34,41 +34,4 @@ private:
     double vMax = 0;
     double aSteps = 0;
     bool autoHomeFlag = false;
-
-// unsigned long computeDelayTicks(
-//     int goalStep,
-//     int currentStep,
-//     int totalDistSteps,
-//     double speed_mm_s,
-//     double steps_per_mm,
-//     double acc_mm_s2
-// ) {
-//     int diff    = goalStep - currentStep;
-//     int absDiff = abs(diff);
-//     int stepsTravelled = totalDistSteps - absDiff;
-//     if (stepsTravelled < 0) stepsTravelled = 0;
-
-//     double vMax   = speed_mm_s * steps_per_mm;   // steps/s
-//     double aSteps = acc_mm_s2 * steps_per_mm;    // steps/s^2
-
-//     if (vMax <= 0.0 || aSteps <= 0.0) return 0; // safe large delay
-
-//     // acceleration cap
-//     double vAcc = sqrt(2.0 * aSteps * (double)stepsTravelled);
-//     if (vAcc < 1.0) vAcc = 1.0;
-
-//     // deceleration cap
-//     double vDec = sqrt(2.0 * aSteps * (double)absDiff);
-//     if (vDec < 1.0) vDec = 1.0;
-
-//     // commanded velocity
-//     double vCmd = std::min(vMax, std::min(vAcc, vDec));
-
-//     // convert to ISR ticks (100us = 1 tick)
-//     double delayTicks = 10000.0 / vCmd;
-//     if (delayTicks < 1.0) delayTicks = 1.0;
-//     if (delayTicks > 1000000.0) delayTicks = 1000000.0;
-
-//     return (unsigned long)delayTicks;
-// }
 };
